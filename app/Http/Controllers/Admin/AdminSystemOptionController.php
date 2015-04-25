77a348ce05150f28e1b4013d1eea7148f85c5a36
raoyc <raoyc2009@gmail.com>
2015-04-26 00:30:02 +0800

    update system_option controller

--- app/Http/Controllers/Admin/AdminSystemOptionController.php
+++ app/Http/Controllers/Admin/AdminSystemOptionController.php
@@ -45,26 +45,14 @@ class AdminSystemOptionController extends BackController
 
     public function putUpdate(Request $request)
     {
-        if (($request->ajax()) && ($request->isMethod('put'))) {
-            $data = $request->input('data');
-            $json = [
-                'status' => 1,
-                'info' => '成功',
-                'operation'=>'更新系统配置',
-                'url' => route('admin.system_option.index'),
-            ];
-            if ($data && is_array($data)) {
-                $this->option->batchUpdate($data);
-                //更新系统静态缓存
-                SystemOptionCache::cacheStatic();
-                return response()->json($json);
-            } else {
-                $json = array_replace($json, ['status' => 0, 'info' => '失败原因为：<span class="text_error">请勿提交非法数据！</span>']);
-                return response()->json($json);
-            }
+        $data = $request->input('data');
+        if ($data && is_array($data)) {
+            $this->option->batchUpdate($data);
+            //更新系统静态缓存
+            SystemOptionCache::cacheStatic();
+            return redirect()->route('admin.system_option.index')->with('message', '成功更新系统配置！');
         } else {
-            //非ajax请求抛出异常
-            return view('back.exceptions.jump', ['exception' => '非法请求，不予处理！']);
+            return redirect()->back()->with('fail', '提交过来的数据异常！');
         }
     }
 }
