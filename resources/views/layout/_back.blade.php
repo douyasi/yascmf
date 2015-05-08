4a549fa27d2576ccd314adce808e2eecd138707b
freehere107 <freehere107@hotmail.com>
2015-05-08 14:33:00 +0800

    git commit

--- resources/views/layout/_back.blade.php
+++ resources/views/layout/_back.blade.php
@@ -1,18 +1,6 @@
 @extends('layout._base')
 
 @section('hacker_header')
-<!--
- __     __         _____   _____  __  __  ______ 
- \ \   / / /\     / ____| / ____||  \/  ||  ____|
-  \ \_/ / /  \   | (___  | |     | \  / || |__   
-   \   / / /\ \   \___ \ | |     | |\/| ||  __|  
-    | | / ____ \  ____) || |____ | |  | || |     
-    |_|/_/    \_\|_____/  \_____||_|  |_||_|     
-                                                 
-    ASCII text from http://patorjk.com/software/taag/#p=display&h=1&v=0&f=Big&t=YASCMF
-    Template from https://almsaeedstudio.com/
-    modified by raoyc<raoyc2009@gmail.com>
--->
 @stop
 
 @section('title') 后台 - YASCMF @stop
@@ -162,14 +150,4 @@ html {overflow-x:hidden;overflow-y:hidden;}/*禁止出现滚动条*/
 
 
 @section('hacker_footer')
-<!--
-______                            _              _                                     _
-| ___ \                          | |            | |                                   | |
-| |_/ /___ __      __ ___  _ __  | |__   _   _  | |      __ _  _ __  __ _ __   __ ___ | |
-|  __// _ \\ \ /\ / // _ \| '__| | '_ \ | | | | | |     / _` || '__|/ _` |\ \ / // _ \| |
-| |  | (_) |\ V  V /|  __/| |    | |_) || |_| | | |____| (_| || |  | (_| | \ V /|  __/| |
-\_|   \___/  \_/\_/  \___||_|    |_.__/  \__, | \_____/ \__,_||_|   \__,_|  \_/  \___||_|
-                                          __/ |
-                                         |___/
--->
 @stop
