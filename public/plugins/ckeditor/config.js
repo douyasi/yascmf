0c3fe79a741fdddf1cf8b003a831ccc01e129b13
raoyc <raoyc2009@gmail.com>
2015-05-22 20:23:23 +0800

    update ckeditor/config.js, fix upload images error

--- public/plugins/ckeditor/config.js
+++ public/plugins/ckeditor/config.js
@@ -10,8 +10,8 @@ CKEDITOR.editorConfig = function( config ) {
 	config.pasteFromWordRemoveStyles = true;  //从word中粘贴内容时是否移除格式 
 	config.forcePasteAsPlainText = true;  //是否强制复制来的内容去除格式,false为不去除 
 	config.image_previewText=' ';
-	config.filebrowserImageUploadUrl = "/assets/lib/ckeditor/plugins/imgupload/imgupload.php"; 
-	config.filebrowserBrowseUrl = "/assets/lib/ckeditor/plugins/imgbrowse/imgbrowse.html?imgroot=/uploads/images/";
+	config.filebrowserImageUploadUrl = "/plugins/ckeditor/plugins/imgupload/imgupload.php"; 
+	config.filebrowserBrowseUrl = "/plugins/ckeditor/plugins/imgbrowse/imgbrowse.html?imgroot=/uploads/images/";
 	config.font_names='宋体/宋体, SimSun;'+'微软雅黑/微软雅黑, Microsoft YaHei;'+'黑体/黑体, SimHei;'+'楷体/楷体, 楷体_GB2312, SimKai;'+'隶书/隶书, SimLi;'+ config.font_names;
 	config.defaultLanguage = 'zh-cn'
 	config.font_defaultLabel = '宋体';
