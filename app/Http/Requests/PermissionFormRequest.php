ebdda38147704465202f8f555c2b3953367b863f
freehere107 <freehere107@hotmail.com>
2015-05-25 16:03:09 +0800

    filter-manager

---
+++ app/Http/Requests/PermissionFormRequest.php
@@ -0,0 +1,29 @@
+<?php namespace Douyasi\Http\Requests;
+
+use Douyasi\Http\Requests\Request;
+//php artisan make:request Users/PermissionFormRequest
+class PermissionFormRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return false;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			//
+		];
+	}
+
+}
