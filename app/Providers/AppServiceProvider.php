3a8df6ec6818e4f0be528021e01315a7e1a02fb7
raoyc <raoyc2009@gmail.com>
2015-06-10 22:02:28 +0800

    laravel 5.1 updated, namespace to new line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,41 +1,30 @@
-<?php namespace Douyasi\Providers;
+<?php
+
+namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Douyasi\Extensions\DouyasiBlade as DouyasiBlade;
 
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /*注册自定义blade标签规则*/
+        DouyasiBlade::register();
+    }
 
-class AppServiceProvider extends ServiceProvider {
-
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		//
-		/*注册自定义blade标签规则*/
-		DouyasiBlade::register();
-	}
-
-	/**
-	 * Register any application services.
-	 *
-	 * This service provider is a great spot to register your various container
-	 * bindings with the application. As you can see, we are registering our
-	 * "Registrar" implementation here. You can add your own bindings too!
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		
-		#Laravel 5 默认绑定认证注册与认证
-		$this->app->bind(
-			'Illuminate\Contracts\Auth\Registrar',
-			'Douyasi\Services\Registrar'
-		);
-		
-	}
-
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
